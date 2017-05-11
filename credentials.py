32db79dfaada3f2844e84addaffc9ea129c9bc42
k33gan <pi@raspberrypi.(none)>
2017-05-11 12:45:42 +0200

    testing

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "Your Account Sid"
-auth_token = "Your Auth Token"
-my_cell = "+Your Cell Number"
-my_twilio = "+Your Twilio Number"
+account_sid = "AC3127b6551815d0dcb5b48da9ea60aab1"
+auth_token = "ac2457d6a0fd28f6eac93297f66b8710"
+my_cell = "+27765074355"
+my_twilio = "+12067450965"
