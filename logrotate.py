d80844792e6b70f89215ba636c75395523b83cbe
k33gan <keegan@saao.ac.za>
2017-05-31 15:05:33 +0200

    Create logrotate.py

---
+++ logrotate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+
+try:
+	filename = "/home/pi/SSS400S/status.log"
+	filename1 = "/var/log/status.log"
+	
+	statinfo = os.stat(filename)
+	if statinfo.st_size > 1000000:
+		os.mv(filename,filename1)
+		target = open(filename, 'w')
+		target.close()
+	else:
+		pass
+
+except KeyboardInterrupt:
+	print "\nKeyboard interrupt detected."
+	target.close()
+
+except:
+        print "Unforseen error occured!"
+        target.close()
+
+finally:
+	target.close()
+	print "Closing Program..."
