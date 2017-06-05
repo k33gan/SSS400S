acd3ac1638b614f869a292ff2f478cb2544e2a32
root <root@raspberrypi>
2017-06-05 18:12:31 +0200

    Latest Status

--- logrotate.py
+++ logrotate.py
@@ -6,21 +6,20 @@ try:
 	filename1 = "/var/log/status.log"
 	
 	statinfo = os.stat(filename)
-	if statinfo.st_size > 1000000:
-		os.mv(filename,filename1)
+	print statinfo.st_size
+	if statinfo.st_size > 1000:
+		os.rename(filename,filename1)
 		target = open(filename, 'w')
 		target.close()
+		os.system("sudo python /home/pi/SSS400S/uploadToGit.py")
 	else:
 		pass
 
 except KeyboardInterrupt:
 	print "\nKeyboard interrupt detected."
-	target.close()
-
+	
 except:
         print "Unforseen error occured during logrotate!"
-        target.close()
-
+        
 finally:
-	target.close()
 	print "Closing Program..."
